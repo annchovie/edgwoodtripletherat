32a4bff090902f2c6807928c45060a193a2d08f3
Anchovy <annegormaley@gmail.com>
2015-02-18 18:38:48 -0500

    crack the safe

--- src/com/harris/challenge/secret_agent_tools/CrackTheSafe.java
+++ src/com/harris/challenge/secret_agent_tools/CrackTheSafe.java
@@ -14,6 +14,9 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.harris.challenge.brata.BrataLauncherActivity;
 import com.harris.challenge.brata.R;
@@ -21,7 +24,9 @@ import com.harris.challenge.brata.R;
 
 public class CrackTheSafe extends Activity{
     AlertDialog dialog;
-    Button buttonStartChallenge;
+    Button buttonStartChallenge, submitButton;
+    TextView decodedMessageView, answerView;
+    EditText a1Edit, a2Edit, a3Edit, a4Edit, a5Edit;
     
     /**
      * Function used for initializing activity variables, such as 
@@ -32,23 +37,11 @@ public class CrackTheSafe extends Activity{
         super.onCreate(savedInstanceState);
         // load the xml layout before initializing widgets
         setContentView(R.layout.activity_crack_the_safe);
-
-        buttonStartChallenge = (Button)findViewById(R.id.buttonCtsStartChallenge);
-        buttonStartChallenge.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /**
-                 * Either of these functions can be used to send and/or get messages from the MasterServer.
-                 * 
-                 * MasterServerCommunicator.getInstructionUsingQR(ActivityName.this);
-                 * MasterServerCommunicator.sendMessageUsingQR(ActivityName.this, message);
-                 * 
-                 * In this case the MasterServer will update your progress and send back a clue that must
-                 * be decoded. 
-                 */
-                MasterServerCommunicator.getInstructionUsingQR(CrackTheSafe.this);
-            }
-        });
+        
+        addStart();
+        addEdits();
+        addSubmit();
+        
     }
     
     /**
@@ -62,6 +55,8 @@ public class CrackTheSafe extends Activity{
     protected void onResume() {
         super.onResume();
         // Here update the correct TextView with MessageDecoder.decodedMessage any time the activity is resumed.
+        decodedMessageView.setText("Decoded: " + MessageDecoder.decodedMessage);
+        
     }
     
     /**
@@ -75,8 +70,39 @@ public class CrackTheSafe extends Activity{
      */
     public String computeCtsAnswer(String clue, int a, int b, int c, int d, int e)
     {
-        String result = null;
+        String result = "No Solution Found";
         // Check result from myHash(a, b, c) until it returns a string matching the 4 character clue 
+        
+        int[] angs = {a,b,c,d,e};
+        
+        for(int i = 0; i<angs.length; i++)
+        {
+        	for(int j = -5; j<6; j++)
+        	{
+        		for(int z = 0; z<angs.length; z++)
+        		{
+        			if(z!=i)
+        			{
+        				int first = angs[i]+j;
+        				for(int q = -5; q<6; q++)
+        				{
+        					int second = angs[z]+q;
+        					for(int f = 0; f<angs.length; f++)
+        					{
+        						if(f!=i&&f!=z)
+        						{
+        							for(int t = -5; t<6; t++ )
+        							{
+        								int third = angs[f]-t;
+        								if(myHash(first, second, third).equals(MessageDecoder.decodedMessage)) return ""+first+", "+second+", "+third;
+        							}
+        						}
+        					}
+        				}
+        			}
+        		}
+        	}
+        }
         return result;
     }
     
@@ -128,4 +154,63 @@ public class CrackTheSafe extends Activity{
             }   
         }, 3000);
     }
+    
+    public void addStart()
+    {
+         decodedMessageView = (TextView) findViewById(R.id.decodedMessageView);
+    	 buttonStartChallenge = (Button)findViewById(R.id.buttonCtsStartChallenge);
+         buttonStartChallenge.setOnClickListener(new Button.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 /**
+                  * Either of these functions can be used to send and/or get messages from the MasterServer.
+                  * 
+                  * MasterServerCommunicator.getInstructionUsingQR(ActivityName.this);
+                  * MasterServerCommunicator.sendMessageUsingQR(ActivityName.this, message);
+                  * 
+                  * In this case the MasterServer will update your progress and send back a clue that must
+                  * be decoded. 
+                  */
+                 MasterServerCommunicator.getInstructionUsingQR(CrackTheSafe.this);
+             }
+         });
+    }
+    
+    public void addEdits()
+    {
+    	a1Edit = (EditText) findViewById(R.id.a1Edit);
+    	a2Edit = (EditText) findViewById(R.id.a2Edit);
+    	a3Edit = (EditText) findViewById(R.id.a3Edit);
+    	a4Edit = (EditText) findViewById(R.id.a4Edit);
+    	a5Edit = (EditText) findViewById(R.id.a5Edit);
+    }
+    
+    public void addSubmit()
+    {
+    	answerView = (TextView) findViewById(R.id.answerView);
+    	submitButton = (Button) findViewById(R.id.submitButton);
+    	submitButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	
+            	int a,b,c,d,e;
+            	
+            	try { 
+            		a = Integer.parseInt(a1Edit.getText().toString());
+	            	b = Integer.parseInt(a2Edit.getText().toString());
+	            	c = Integer.parseInt(a3Edit.getText().toString());
+	            	d = Integer.parseInt(a4Edit.getText().toString());
+	            	e = Integer.parseInt(a5Edit.getText().toString());
+	            	
+	            	answerView.setText("Combo: " + computeCtsAnswer(MessageDecoder.decodedMessage, a, b, c, d, e));
+            	}
+            	catch(NumberFormatException error ){
+            		 Toast.makeText(getApplicationContext(), "Error with Parse Int", Toast.LENGTH_LONG).show();
+            	}
+            	
+                
+            }
+        });
+    }
+  
 }
